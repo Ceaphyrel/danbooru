129260b22bb910fc0c80d94f518d3f08ee1cecbb
Ceaphyrel <ceaphyrel@gmail.com>
2018-01-07 14:32:08 +0100

    Configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
-set :user, "albert"
+set :user, "ceaphyrel"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "ceaphyrel.sytes.net", :roles => %w(web app db), :primary => true, :user => "ceaphyrel"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
